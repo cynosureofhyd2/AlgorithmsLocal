eb78e3730b71fa7690231b21048053ca06901ba6
Prashanth_King <tomu646@gmail.com>
2013-06-09 11:23:55 -0700

    GetNth in a Linked List

--- BlankProject-vs2008/Blank Project/LinkedList.cpp
+++ BlankProject-vs2008/Blank Project/LinkedList.cpp
@@ -2,8 +2,84 @@
 #include <iostream>
 #include "console.h"
 using namespace std;
+#include "random.h"
+
+struct node {
+	int data;
+	struct node* next;
+};
+
+static node* CreateNewNode(int data)
+{
+	struct node* newNode = (struct node*)malloc(sizeof(struct node));
+	newNode->data = data;
+	newNode->next = NULL;
+	return newNode;
+}
+
+static void push(struct node*& headRef, int data)
+{
+	struct node* newNode = CreateNewNode(data);
+	newNode->next = headRef;
+	headRef = newNode;
+}
+
+struct node* CreateRandomList(int number)
+{
+	int i = 0; 
+	struct node* newList = NULL;
+	while(i < number)
+	{
+		struct node* newNode = CreateNewNode(randomInteger(1, number));
+		push(newList, newNode->data);
+		i++;
+	}
+	return newList;
+}
+
+static int Length(struct node*& head)
+{
+	int count = 0;
+	struct node* current = head;
+	if(head == NULL)
+		return 0;
+	else
+	{
+		while(current != NULL)
+		{
+			current = current->next;
+			count++;
+		}
+	}
+	return count;
+}
+
+int GetNth(struct node* list, int N)
+{
+	struct node* current = list;
+
+	int counter = 0;
+	if(list == NULL)
+		return -100;
+	if(N < 0)
+		return -100;
+	else
+	{
+		while(current != NULL && counter < N)
+		{
+			current = current->next;
+			counter++;
+		}
+	}
+	return current->data;
+}
 
 int main()
-{
+{	
+	struct node* newList  = CreateRandomList(10);
+	cout << Length(newList) << endl;
+	int nthNode = GetNth(newList, 5);
+	cout << nthNode << endl;
 	return 0;
 }
+
