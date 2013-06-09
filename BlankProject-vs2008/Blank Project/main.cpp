87dee8dc843a260b51368ffd94917a22fcc8115b
Prashanth_King <tomu646@gmail.com>
2013-06-08 17:08:26 -0700

    Add the Blank Solution in VS C++ 08

---
+++ BlankProject-vs2008/Blank Project/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * File: main.cpp
+ * --------------
+ * This is a blank C++ source file.
+ */
+
+#include <iostream>
+#include "console.h"
+using namespace std;
+
+int main() {
+    return 0;
+}
